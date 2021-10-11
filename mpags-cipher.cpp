84a4ed4811153e23792886c15c94b8ca54f95f45
Cwu21 <cxw191@student.bham.ac.uk>
2021-10-11 13:03:28 +0000

    mpags-cipher.cpp

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
-int main()
-{
- // This is a comment
- /* This is a
- Multiline comment */
+#include <string>
+#include <vector>
 
- std::cout << "Hello World!\n";
+int main(int argc, char* argv[])
+{
+    const std::vector<std::string> cmdLineArgs { argv, argv+argc };
+
+    for (size_t i{1}; i<cmdLineArgs.size() ;i++)
+    {
+        if ((cmdLineArgs[i] == "-h")||(cmdLineArgs[i] == "--help"))
+        {
+            std::cout << "help requested" << std::endl;
+        }
+        else if(cmdLineArgs[i] == "-i"||cmdLineArgs[i] == "-o")
+        {
+            i++;
+            std::string filename;
+            filename = cmdLineArgs[i];
+            std::cout << filename << std::endl;
+        }
+        else if (cmdLineArgs[i] == "--version")
+        {
+           std::cout << __cplusplus << std::endl;
+        }
+        else 
+        {
+            std::cout << cmdLineArgs[i] << std::endl;
+        }
+        
+    }
+
 }
